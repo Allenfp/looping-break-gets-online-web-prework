d3664c35568bf69e65c22483ffc18d5339a241d5
Allen F Partlow <allenfp@gmail.com>
2019-01-19 21:04:16 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	#your code here
+  loop do
+    puts "What is the spell that enacts levitation?"
+    name = $stdin.gets.chomp
+    if name == "Wingardium Leviosa"
+      puts "You passed the quiz!"
+      break
+    end
+  end
 end
-
-
